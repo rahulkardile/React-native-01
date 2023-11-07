5e5cb7b20b3879c0c03cbf16489f45ae1b002902
Rahul kardile <rahulkardile321@gmail.com>
2023-11-08 03:35:42 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  
+  const Incraese = ()=> {
+    setCount(count + 1)
+  }
+
+  const Decrease = ()=> {
+    setCount( count - 1)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Click Here to increase a count!</Text>
+      <Text style={styles.text}> Your Total Count is {count}</Text>
+      <Button onPress={() => Incraese()} style={styles.button} title='Click me To Increase count' />
+      <Button onPress={() => Decrease()} style={styles.button} title='Click me To Increase count' />
     </View>
   );
-}
+};
 
+// React Native Styles
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'dark',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  text: {
+    paddingTop: 10,
+    marginBottom: 10,
+  },
+  button: {
+    color: 'red',
+    backgroundColor: 'red'
+  }
 });
+
+export default App
